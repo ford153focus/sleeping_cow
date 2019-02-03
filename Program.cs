260c6287040c9864bf299ddcab76d85766d6a2d3
Joe Ford <focus@ford-rt.com>
2019-02-03 10:54:36 +0300

    move time correction to separate method

--- Program.cs
+++ Program.cs
@@ -11,25 +11,33 @@ namespace sleeping_cow
         {
             Console.WriteLine("Hello World!");
             bannedTasksKiller();
+            timeCorrector();
+        }
 
+        private static void timeCorrector()
+        {
             var ntpTime = Utils.DateTimeUtils.Ntp.GetNetworkTime();
             Utils.DateTimeUtils.SetSystemDateTime.SetByDtObj(ntpTime);
             Utils.DateTimeUtils.SetSystemTimeZone("Russian Standard Time");
 
             if (DateTime.Now.Hour >= 0 && DateTime.Now.Hour <= 5) {
-                Utils.PowerUtils.sleep();
+                Utils.PowerUtils.shutDown();
             }
         }
 
         private static void bannedTasksKiller()
         {
-            var bannedTasks = new List<string> { "left4dead", "left4dead2", "LeagueClient" };
+            var bannedTasks = new List<string> { 
+                "left4dead", 
+                "LeagueClient" 
+            };
 
             foreach (Process theProcess in Process.GetProcesses())
             {
                 if (bannedTasks.Contains(theProcess.ProcessName))
                 {
-                    theProcess.Kill();
+                    // theProcess.Kill();
+                    Utils.Bsod.Invoke();
                 }
             }
         }
